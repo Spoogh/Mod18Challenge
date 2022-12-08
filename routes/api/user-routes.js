11b72add20d03fd42db5ad1d7767bd33e9f9e5cf
Spoogh <joshuafloresjjf@gmail.com>
2022-12-08 00:30:48 -0600

    added routes, connection and server code

---
+++ routes/api/user-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+   getAllUsers,
+   getUserById,
+   createUser,
+   updateUser,
+   deleteUser,
+   addFriend,
+   removeFriend
+} = require('../../controllers/user-controller');
+const {remove} = require('../../models/User');
+
+//set up get all and post 
+router
+.route('/')
+.get(getAllUsers)
+.post(createUser);
+
+
+router
+.route('/:id')
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+router
+.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(removeFriend)
+
+module.exports = router;
